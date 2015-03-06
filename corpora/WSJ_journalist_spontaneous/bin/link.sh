edf657ecd064764a864a63a716b6caf0fb6e7b43
xcao <ngafrance@gmail.com>
2015-03-06 16:42:14 +0100

    link.sh v3

--- corpora/WSJ_journalist_spontaneous/bin/link.sh
+++ corpora/WSJ_journalist_spontaneous/bin/link.sh
@@ -1,3 +1,3 @@
-mkdir ../data/
-mkdir ../logs/
+mkdir ../corpora/WSJ_journalist_read/data/
+mkdir ../corpora/WSJ_journalist_read/logs/
 ln -s /fhgfs/bootphon/data/derived_data/WSJ_abkhazia/WSJ_journalist_read/data ../data/
