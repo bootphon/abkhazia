fa2998d88e09f865d5d0ffb03f4a0687b23e0ef7
xcao <ngafrance@gmail.com>
2015-03-06 16:23:45 +0100

    link.sh v2

--- corpora/WSJ_all_read/bin/link.sh
+++ corpora/WSJ_all_read/bin/link.sh
@@ -1,1 +1,3 @@
-ln -s /fhgfs/bootphon/data/derived_data/WSJ_abkhazia/WSJ_all_read/data ../data/
+mkdir ../data/
+mkdir ../logs/
+ln -s /fhgfs/bootphon/data/derived_data/WSJ_abkhazia/WSJ_journalist_read/data ../data/
--- corpora/WSJ_journalist_read/bin/link.sh
+++ corpora/WSJ_journalist_read/bin/link.sh
@@ -1,1 +1,3 @@
+mkdir ../data/
+mkdir ../logs/
 ln -s /fhgfs/bootphon/data/derived_data/WSJ_abkhazia/WSJ_journalist_read/data ../data/
