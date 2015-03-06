2a6de6ee4af5544d6762fc3e79f95e173487bad0
Thomas Schatz <thomas.schatz@laposte.net>
2015-03-06 14:26:08 +0100

    Added link to wiki doc in README.md

---
+++ bin/kaldi/abkhazia2kaldi.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  5 11:32:55 2015
+
+@author: Thomas Schatz
+"""
+
+"""
+This script converts a corpus in abkhazia format to standard kaldi format
+"""
+
