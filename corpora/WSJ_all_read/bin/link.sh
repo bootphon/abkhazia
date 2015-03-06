1b57d42b696fa1d17558a6246477cb1796f4dd2c
xcao <ngafrance@gmail.com>
2015-03-06 17:11:50 +0100

    link.sh v6

--- corpora/WSJ_all_read/bin/link.sh
+++ corpora/WSJ_all_read/bin/link.sh
@@ -1,2 +1,1 @@
-ln -s /fhgfs/bootphon/data/derived_data/WSJ_abkhazia/WSJ_journalist_read/data/wavs ../data/wavs
-ln -s /fhgfs/bootphon/data/derived_data/WSJ_abkhazia/WSJ_journalist_read/data ../data/
+ln -s /fhgfs/bootphon/data/derived_data/WSJ_abkhazia/WSJ_journalist_read/data ../
