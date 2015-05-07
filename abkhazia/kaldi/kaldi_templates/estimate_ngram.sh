147d22e33ed94e5fad15d5c885d4dbdd890e057c
Thomas Schatz <thomas.schatz@laposte.net>
2015-05-07 10:48:07 +0200

    Debugging n-gram LM support for kaldi recipes

--- abkhazia/kaldi/kaldi_templates/estimate_ngram.sh
+++ abkhazia/kaldi/kaldi_templates/estimate_ngram.sh
@@ -16,9 +16,10 @@
 # limitations under the License.
 
 # Estimates a simple N-gram language model
+# This uses utils/format_lm_sri.sh from kaldi which might be limited to 3-grams models?
 
 # example usage with standard train_and_decode recipe
-#local/estimate_ngrams.sh data/train/text data/lang data/test/text
+#local/estimate_ngram.sh data/train/text data/lang_test data/test/text
 
 # Doesn't do anything specific to avoid overfitting
 # if this become a problem, could use pruning:
@@ -49,6 +50,8 @@ model_order=2
 
 . path.sh || { echo "Cannot source path.sh"; exit 1; }
 
+mkdir -p out_dir
+
 # train (use IRSTLM)
 if [ $remove_first_col = true ]; then
   set -eu  # stop on error
@@ -59,7 +62,7 @@ fi
 add-start-end.sh < "$out_dir"/train > "$out_dir"/train_se
 # k option is number of split, useful for huge text files
 build-lm.sh -i "$out_dir"/train_se -n $model_order -o "$out_dir"/train.ilm.gz -k 1 -s kneser-ney
-compile-lm train.ilm.gz --text=yes /dev/stdout | gzip -c > "$out_dir"/train.arpa.gz
+compile-lm "$out_dir"/train.ilm.gz --text=yes /dev/stdout | gzip -c > "$out_dir"/train.arpa.gz
 
 # test (use IRSTLM)
 if [ $remove_first_col = true ]; then
@@ -76,7 +79,14 @@ compile-lm "$out_dir"/train.arpa.gz --eval="$out_dir"/test_se
 # might want to make data/lang a parameter if we want to be able to use this to estimate
 # n-grams for models using a different lexicon, like phone loops (unless we change the training
 # instead ?)
-utils/format_lm_sri.sh data/lang "$out_dir"/train.arpa.gz data/local/dict/lexicon.txt $out_dir
+
+# srilm_opts: do not use -tolower by default, since we do not make assumption that lexicon
+# has no meaningful lowercase/uppercase distinctions (and it can be in unicode, in which
+# case I have no idea what lowercasing would produce)
+utils/format_lm_sri.sh 	--srilm_opts="-subset -prune-lowprobs -unk" \
+	data/lang "$out_dir"/train.arpa.gz \
+	data/local/dict/lexicon.txt $out_dir \
+
 
 # clean
 rm "$out_dir"/train
