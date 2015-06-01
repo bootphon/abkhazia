128fa4048da75aa407204470d571b469004643ba
Thomas Schatz <thomas.schatz@laposte.net>
2015-06-02 01:38:52 +0200

    Debugging yaafe features extraction for abkhazia

--- abkhazia/utilities/features.py
+++ abkhazia/utilities/features.py
@@ -36,7 +36,7 @@ def segment_features(features_file, segments_file, out_file):
 				# select features for appropriate segment
 				utt_ids.append(utt_id)
 				indices = np.where(np.logical_and(times >= start, times <= stop))[0]
-				if indices.size() == 0:
+				if indices.size == 0:
 					print start
 					print stop
 					print times
