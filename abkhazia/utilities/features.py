74c98333ea8f0a492cfcc1cb5b4e93736230f0d9
Thomas Schatz <thomas.schatz@laposte.net>
2015-06-02 01:41:15 +0200

    Debugging yaafe features extraction for abkhazia

--- abkhazia/utilities/features.py
+++ abkhazia/utilities/features.py
@@ -36,11 +36,10 @@ def segment_features(features_file, segments_file, out_file):
 				# select features for appropriate segment
 				utt_ids.append(utt_id)
 				indices = np.where(np.logical_and(times >= start, times <= stop))[0]
-				if indices.size == 0:
-					print start
-					print stop
-					print times
-				utt_times = times[indices] - start  # get times relative to beginning of utterance
+				print start
+				print stop
+				print times
+				utt_times.append(times[indices] - start)  # get times relative to beginning of utterance
 				utt_features.append(features[indices,:])
 			# write to out_file once for each wavefile
 			h5features.write(out_file, 'features', utt_ids, utt_times, utt_features)
