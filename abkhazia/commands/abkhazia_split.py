93bea090510656faef12981912af485a2b0e8cf9
Mathieu Bernard <mathieu_bernard@laposte.net>
2016-02-21 21:10:52 +0100

    new 'abkhazia split' command

--- abkhazia/commands/abkhazia_split.py
+++ abkhazia/commands/abkhazia_split.py
@@ -14,9 +14,91 @@
 # along with abkahzia. If not, see <http://www.gnu.org/licenses/>.
 """Implementation of the 'abkazia split' command"""
 
+import argparse
+import sys
+
+import abkhazia.utils.split as split
+
 class AbkhaziaSplit(object):
+    '''This class implemnts the 'abkahzia split' command
+
+    Basically this class defines an argument parser, parses the
+    arguments and split a corpus in train and test subsets. The
+    spliting operation is delegated to the split.SplitCorpus class.
+
+    '''
     name = 'split'
-    description = 'Split a corpus in train and test sets'
+    description = 'Split a corpus in train and test subsets'
 
     def __init__(self):
-        raise NotImplementedError('split command not implemented')
+        # parse the arguments (ignore the first and second which are
+        # 'abkahzia split')
+        args = self.parser().parse_args(sys.argv[2:])
+
+        # instanciate a SplitCorpus instance
+        spliter = split.SplitCorpus(
+            args.input_dir, args.output_dir, args.random_seed, args.verbose)
+
+        # choose the split function according to --by-speakers
+        split_fun = (spliter.split_by_speakers if args.by_speakers
+                     else spliter.split)
+
+        # split the corpus and write it to the output directory
+        split_fun(args.test_prop, args.train_prop)
+
+    @classmethod
+    def parser(cls):
+        """Return a parser for the split command"""
+        parser = argparse.ArgumentParser(
+            prog='abkhazia split',
+            usage='%(prog)s <input-dir> [--output-dir OUTPUT_DIR]\n'
+            + ' '*23 + ('\n' + ' '*23).join([
+                '[--help] [--verbose] [--by-speakers] [--random-seed SEED]',
+                '[--test-prop TEST|--train-prop TRAIN']),
+            description=cls.description)
+
+        group = parser.add_argument_group('directories')
+
+        group.add_argument(
+            'input_dir', metavar='input-dir',
+            help='root directory of the abkhazia corpus to split')
+
+        group.add_argument(
+            '-o', '--output-dir', default=None,
+            help='output directory of the splited corpus, '
+            'if not specified use input-dir/split')
+
+        parser.add_argument(
+            '-v', '--verbose', action='store_true',
+            help='display more messages to stdout')
+
+        group = parser.add_argument_group('split arguments')
+
+        group.add_argument(
+            '-b', '--by-speakers', action='store_true',
+            help='if specified, the data for each speaker is attributed '
+            'either to the test or train subset as a whole. If not specified, '
+            'data from a same speaker is randomly splitted in the two subsets')
+
+        group.add_argument(
+            '-r', '--random-seed', default=None, metavar='SEED',
+            help='seed for pseudo-random numbers generation (default is to '
+            'use the current system time)')
+
+        prop = group.add_mutually_exclusive_group()
+        prop.add_argument(
+            '-t', '--test-prop', default=None, type=float, metavar='TEST',
+            help='''a float between 0.0 and 1.0, represent the proportion of the
+            dataset to include in the test set. If not specfied, the
+            value is automatically set to the complement of the train
+            size. If train size is not specified, test size is set to
+            0.5.''')
+
+        prop.add_argument(
+            '-T', '--train-prop', default=None, type=float, metavar='TRAIN',
+            help='''a float between 0.0 and 1.0, represent the proportion of the
+            dataset to include in the train set. If not specified, the
+            value is automatically set to the complement of the test
+            size.''')
+
+        return parser
---
+++ abkhazia/utils/split.py
@@ -0,0 +1,259 @@
+# Copyright 2016 Thomas Schatz, Xuan Nga Cao, Mathieu Bernard
+#
+# This file is part of abkhazia: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Abkhazia is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with abkahzia. If not, see <http://www.gnu.org/licenses/>.
+"""Provides a class to split an abkahzia corpus in train and test subsets"""
+
+import os
+import random
+import shutil
+
+import abkhazia.utils as utils
+import abkhazia.utils.basic_io as io
+
+class SplitCorpus(object):
+    """A class for spliting an abkahzia corpus into train and test subsets
+
+    corpus_dir : The root directory of the abkhazia corpus to
+      split. This directory must contain a validated abkahzia corpus.
+
+    output_dir : The output directory where to write the splits. Two
+      subdirectories are created (test and train) as well as a log
+      file. This output directory must not be an existing one.
+
+    random_seed : Seed for pseudo-random numbers generation
+      (default is None and current system time is used)
+
+    verbose : This argument serves as initialization of the log2file
+      module. See there for more documentation.
+
+    """
+    default_test_prop = 0.5
+
+    def __init__(self, corpus_dir,
+                 output_dir=None, random_seed=None, verbose=False):
+        # init the corpus directory
+        if not os.path.isdir(corpus_dir):
+            raise OSError('{} is not a directory'.format(corpus_dir))
+        self.data_dir = os.path.join(corpus_dir, 'data')
+        if not os.path.isdir(self.data_dir):
+            raise OSError('{} is not a directory'.format(self.data_dir))
+
+        # all we need from the corpus is the utt2spk.txt file
+        utt2spk_file = os.path.join(self.data_dir, 'utt2spk.txt')
+        if not os.path.exists(utt2spk_file):
+            raise OSError('{} file does not exist'.format(utt2spk_file))
+
+        # init the output directory
+        if output_dir is None:
+            output_dir = os.path.join(corpus_dir, 'split')
+
+        # init output_dir/{test, train}
+        self.test_dir = os.path.join(output_dir, 'test')
+        self.train_dir = os.path.join(output_dir, 'train')
+        for path in (self.test_dir, self.train_dir):
+            if not os.path.exists(path):
+                os.makedirs(path)
+
+        # init the log system
+        self.log = utils.log2file.get_log(
+            os.path.join(output_dir, 'split_corpus.log'), verbose)
+
+        # seed the random generator
+        if random_seed is not None:
+            self.log.debug('random seed is {}'.format(random_seed))
+        random.seed(random_seed)
+
+        # read the utt2spk file from the input corpus
+        self.log.info('reading from {}'.format(utt2spk_file))
+        utt_ids, utt_speakers = io.read_utt2spk(utt2spk_file)
+        self.utts = zip(utt_ids, utt_speakers)
+        self.size = len(utt_ids)
+        self.speakers = set(utt_speakers)
+        self.log.debug('loaded {} utterances from {} speakers'
+                       .format(self.size, len(self.speakers)))
+
+
+    def _write(self, train_utt_ids, test_utt_ids):
+        """Write the train and test split corpus to the output directory
+
+        Before writing the files, this finction also sort them (with
+        respect to the first comulmn)
+
+        """
+        self.log.info('splitting proportions are {} for train and {} for test'
+                       .format(round(float(len(train_utt_ids))/self.size, 3),
+                               round(float(len(test_utt_ids))/self.size, 3)))
+
+        self.log.info('writing to {}'.format(
+            os.path.abspath(os.path.join(self.test_dir, '..'))))
+
+        # for train and test subsets
+        for target_dir, utts in [(self.train_dir, train_utt_ids),
+                                 (self.test_dir, test_utt_ids)]:
+            # take subparts of utt2spk.txt, text.txt and segments.txt
+            for filein in (['utt2spk.txt', 'text.txt', 'segments.txt']):
+                target = os.path.join(target_dir, filein)
+                try:
+                    self.log.debug('writing {}'.format(target))
+                    io.copy_first_col_matches(
+                        os.path.join(self.data_dir, filein),
+                        target, utts)
+                    io.cpp_sort(target)
+                except:  # TODO a bit paranoid, no ?
+                    try:
+                        utils.remove(target)
+                    except shutil.Error:
+                        self.log.error("can't delete {}".format(self.train_dir))
+                    finally:
+                        raise OSError('cannot writing to {}'.format(target))
+
+    def _proportions(self, test_prop, train_prop):
+        """Return 'regularized' proprotions of test and train data
+
+        Return the tuple (test_prop, train_prop), ensures they are in
+        [0, 1] and their sum is 1. If None, return the default values.
+
+        """
+        # set default proportion values
+        if test_prop is None:
+            test_prop = (self.default_test_prop
+                         if train_prop is None else 1 - train_prop)
+        if train_prop is None:
+            train_prop = 1 - test_prop
+
+        if test_prop < 0 or test_prop > 1:
+            raise RuntimeError('test proportion must be in [0, 1]')
+        if train_prop < 0 or train_prop > 1:
+            raise RuntimeError('train proportion must be in [0, 1]')
+
+        self.log.debug('proportion for train is {}'.format(train_prop))
+        self.log.debug('proportion for test is {}'.format(test_prop))
+
+        if 1 != test_prop + train_prop:
+            raise RuntimeError(
+                'sum of test and train proportion is not 1')
+
+        return test_prop, train_prop
+
+
+    def split(self, test_prop=None, train_prop=None):
+        """Split the corpus by utterances regardless of the speakers
+
+        Both generated subsets get speech from all the speakers with a
+        number of utterances by speaker in each set matching the
+        number of utterances by speaker in the whole corpus.
+
+        test_prop : float, should be between 0.0 and 1.0 and
+          represent the proportion of the dataset to include in the
+          test split. If None, the value is automatically set to the
+          complement of the train size. If train size is also None,
+          test size is set to 0.5. (default is None)
+
+        train_prop : float, should be between 0.0 and 1.0 and
+          represent the proportion of the dataset to include in the
+          train split. If None, the value is automatically set to the
+          complement of the test size. (default is None)
+
+        """
+        test_prop, train_prop = self._proportions(test_prop, train_prop)
+
+        train_utt_ids = []
+        test_utt_ids = []
+        for speaker in self.speakers:
+            spk_utts = [utt_id for utt_id, utt_speaker in self.utts
+                        if utt_speaker == speaker]
+
+            if len(spk_utts) <= 1:
+                raise RuntimeError(
+                    'Speaker {} has only {} sentence'
+                    .format(speaker, len(spk_utts)))
+
+            n_train = int(round(len(spk_utts) * train_prop))
+
+            self.log.debug('spliting {} utterances from speaker {} -> '
+                           '{} for train, {} for test'
+                           .format(len(spk_utts), speaker,
+                                   n_train, len(spk_utts) - n_train))
+
+            # sample train and test utterances at random for this speaker
+            train_utts = random.sample(spk_utts, n_train)
+            test_utts = list(set.difference(set(spk_utts), set(train_utts)))
+
+            # add to train and test sets
+            train_utt_ids += train_utts
+            test_utt_ids += test_utts
+
+        self._write(train_utt_ids, test_utt_ids)
+
+
+    def split_by_speakers(self, test_prop=None, train_prop=None):
+        """Split the corpus by speakers
+
+        Generated train and test subsets get speech from different
+        speakers and the data for each speaker is attributed to one of
+        the two sets as a whole.
+
+        Note that this might not be appropriate when the amount of
+        utterances available per speaker is too unbalanced.
+
+        test_prop : If float, should be between 0.0 and 1.0 and
+          represent the proportion of the dataset to include in the
+          test split. If int, represents the absolute number of test
+          samples. If None, the value is automatically set to the
+          complement of the train size. If train size is also None,
+          test size is set to 0.5. (default is None)
+
+        train_prop : If float, should be between 0.0 and 1.0 and
+          represent the proportion of the dataset to include in the
+          train split. If int, represents the absolute number of train
+          samples. If None, the value is automatically set to the
+          complement of the test size. (default is None)
+
+        """
+        test_prop, train_prop = self._proportions(test_prop, train_prop)
+
+        # randomize the speakers
+        speakers = list(self.speakers)
+        random.shuffle(speakers)
+
+        # split from a subpart of the randomized speakers
+        n_train_speakers = int(round(train_prop * len(self.speakers)))
+        self.split_from_speakers_list(speakers[:n_train_speakers])
+
+
+    def split_from_speakers_list(self, train_speakers):
+        """Split the corpus from a list of speakers in the train set"""
+
+        unknown = [spk for spk in train_speakers if spk not in self.speakers]
+        if unknown != []:
+            raise RuntimeError(
+                "The following speakers specified in train_speakers "
+                "are not found in the corpus: {}".format(unknown))
+
+        train_utt_ids = []
+        test_utt_ids = []
+        for speaker in self.speakers:
+            spk_utts = [utt_id for utt_id, utt_speaker in self.utts
+                        if utt_speaker == speaker]
+
+            if speaker in train_speakers:
+                train_utt_ids += spk_utts
+                msg = 'train'
+            else:
+                test_utt_ids += spk_utts
+                msg = 'test'
+            self.log.debug('{} utterances from speaker {} -> {}'
+                           .format(len(spk_utts), speaker, msg))
+
+        self._write(train_utt_ids, test_utt_ids)
