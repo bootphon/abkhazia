028d80e362ae18a8ff33491a81c4311ba8b4bc02
Mathieu Bernard <mathieu_bernard@laposte.net>
2016-09-19 15:13:33 +0200

    added make_transcription to CID preparator

--- abkhazia/corpus/prepare/cid_preparator.py
+++ abkhazia/corpus/prepare/cid_preparator.py
@@ -16,7 +16,6 @@
 """Data preparation for the CID corpus"""
 
 import os
-import sys
 
 import abkhazia.utils as utils
 from abkhazia.corpus.prepare import AbstractPreparator
@@ -29,16 +28,16 @@ class CIDPreparator(AbstractPreparator):
     description = 'Corpus of Interactional Data'
 
     long_description = '''
+    Le CID (Corpus d'interactions dialogales / Corpus of Interactional
+    Data) est un corpus audio-video de 8 heures, en français, destiné
+    à l'annotation multimodale qui inclut la phonétique, la prosodie,
+    la morphologie, la syntaxe, le discours et la mimo-gestualité.
     '''
 
-    url = ''
+    url = 'https://www.ortolang.fr/market/corpora/sldr000720'
     audio_format = 'wav'
 
-    # IPA transcriptions for all phones in the Buckeye corpus. The
-    # following phones are never found in the transcriptions: set([u'own',
-    # u'ahn', u'ihn', u'ayn', u'NSN', u'eyn', u'oyn', u'ehn', u'iyn',
-    # u'B', u'E', u'uhn', u'aon', u'awn', u'uwn', u'aan', u'ern', u'aen'])
-    # Reason: we already collapsed them in the foldings_version
+    # IPA transcriptions for all phones in the CID corpus.
     phones = {
         '@': u'ə',
         'a~': u'ɑ̃',
@@ -68,8 +67,7 @@ class CIDPreparator(AbstractPreparator):
         'w': u'w',
         'y': u'y',
         'z': u'z',
-        'Z': u'ʒ',
-
+        'Z': u'ʒ'
     }
 
     silences = [u"SIL_WW", u"NSN"]  # SPN and SIL will be added automatically
@@ -82,7 +80,12 @@ class CIDPreparator(AbstractPreparator):
         self.copy_wavs = copy_wavs
 
     def _yield_transcription(self, trs_file):
-        """Yield (utt, wav, text, tstart, tstop) read from transcription"""
+        """Yield (utt, wav, text, tstart, tstop) read from transcription
+
+        Utterances containing only gpf_* are ignored as they contain
+        no clean speech
+
+        """
         # speaker id are the first 2 letters of the file
         spk = os.path.basename(trs_file)[:2]
 
@@ -96,11 +99,12 @@ class CIDPreparator(AbstractPreparator):
             elif line.startswith('xmax'):
                 tstop = float(line.split(' = ')[1])
             elif line.startswith('text'):
-                text = line.split(' = ')[1]
+                text = line.split(' = ')[1].replace('"', '')
 
-            if utt and tstart and tstop:
-                yield utt, spk + '-anonym', text, tstart, tstop
-                utt, tstart, tstop, text = None, None, None, None
+            if utt and tstart and tstop and text:
+                if not text.startswith('gpf_'):
+                    yield utt, spk + '-anonym', text, tstart, tstop
+                    utt, tstart, tstop, text = None, None, None, None
 
     def list_audio_files(self):
         return utils.list_files_with_extension(
@@ -113,8 +117,8 @@ class CIDPreparator(AbstractPreparator):
         trs_files = (t for t in utils.list_directory(
             os.path.join(self.input_dir, 'TextGrid'), abspath=True)
                      if 'transcription' in os.path.basename(t))
-        for trs_file in trs_files:
-            for utt, wav, _, tstart, tstop in self._yield_transcription(trs_file):
+        for trs in trs_files:
+            for utt, wav, _, tstart, tstop in self._yield_transcription(trs):
                 segments[utt] = (wav, tstart, tstop)
         return segments
 
@@ -122,21 +126,16 @@ class CIDPreparator(AbstractPreparator):
         return {k: k[:2] for k in self.make_segment().iterkeys()}
 
     def make_transcription(self):
-        pass
-        # text = dict()
-        # for utts in self._list_files('.txt', exclude=['readme']):
-        #     bname = os.path.basename(utts)
-        #     utt = os.path.splitext(bname)[0]
-        #     for idx, line in enumerate(
-        #             (l.strip() for l in open(utts).readlines()
-        #              if len(l.strip())), start=1):
-        #         text[utt + '-sent' + str(idx)] = line
+        text = dict()
 
-        # # one utterance have "k p's" in text, where "k p" is an
-        # # acronym in this context. Because "p's" is processed as OOV, we
-        # # simply replace it by "p"
-        # text['s2202b-sent15'] = text['s2202b-sent15'].replace("p's", "p")
-        # return text
+        # transcription files are CID/TextGrid/*transcription*.TextGrid
+        trs_files = (t for t in utils.list_directory(
+            os.path.join(self.input_dir, 'TextGrid'), abspath=True)
+                     if 'transcription' in os.path.basename(t))
+        for trs_file in trs_files:
+            for utt, _, txt, _, _ in self._yield_transcription(trs_file):
+                text[utt] = txt
+        return text
 
     def make_lexicon(self):
         pass
