e8717e530b605f5b31ac226a5d495251c93782e4
Mathieu Bernard <mathieu_bernard@laposte.net>
2019-11-09 11:42:24 +0100

    bugfix in install

--- setup.py
+++ setup.py
@@ -44,11 +44,6 @@ setup(
     zip_safe=False,
     scripts=[],
 
-    # install dependancies from bootphon github
-    dependency_links=[
-        GITHUB_BOOTPHON +
-        'h5features/archive/v1.3.2.tar.gz#egg=h5features-1.3.2'],
-
     # install python dependencies from PyPI
     install_requires=REQUIREMENTS,
 
