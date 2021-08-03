2b80ae393d0404fbd2c1bca7300b12fdff0e855f
mkhentout <manelkhentoutmk@gmail.com>
2021-08-03 12:23:34 +0200

    slurm job script

---
+++ abkhazia/corpus/prepare/aerc_job.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=aesrc             # Job name
+#SBATCH --partition=all              # Take a node from the 'cpu' partition
+
+
+#SBATCH --output=%x-%j.log            # Standard output and error log
+
+echo "Running job on $hostname"#?
+
+# load conda environment
+source /shared/apps/anaconda3/envs/abkhazia
+conda activate abkhazia
+
+# launch your computation
+python aesrc_dev.py # input_path output_path
