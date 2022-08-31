fedaafa8a929ceebb93b04df6858b697f02e6ef1
Manel khentout <35903456+manelkhentout@users.noreply.github.com>
2022-08-31 16:08:14 +0200

    cleanup

--- abkhazia/corpus/prepare/aesrc_dev.py
+++ abkhazia/corpus/prepare/aesrc_dev.py
@@ -11,11 +11,10 @@ log = get_log(verbose=True)
 # create the Buckeye Preparator
 preparator = AESRCPreparator(
    '/home/mkhentout/Bureau/Dataset/abkhazia/American English Speech Data',
-   #'/scratch1/data/raw_data/AESRC/Datatang-English/data/American English Speech Data'
-
+   #dataset_path :'/scratch1/data/raw_data/AESRC/Datatang-English/data/American English Speech Data'
     copy_wavs=False,log=log)
-#/home/mkhentout/Bureau/Dataset/Datatang-English/data/American English Speech Data
-# prepare the Buckeye corpus into abkhazia format (using a temp directory to
+
+# prepare the aesrc corpus into abkhazia format (using a temp directory to
 # store symlinks to the wavs)
 with tempfile.TemporaryDirectory() as tmpdir:
     corpus = preparator.prepare(tmpdir)
